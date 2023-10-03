cbc1acf96bf65a38df40bc7d060fff7a8749b3ad
Basil8943 <108082333+Basil8943@users.noreply.github.com>
2023-10-03 13:27:02 +0530

    Update Dall_E2.py

--- Dall_E2.py
+++ Dall_E2.py
@@ -13,7 +13,7 @@ global_message ="Please enter the password to continue..!"
 def check_password():
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"].get("password");
+        if st.session_state["password"] == st.secrets["passwords"]["password"]:
             st.session_state["password_correct"] = True
             del st.session_state["password"]
         else:
